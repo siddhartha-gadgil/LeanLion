04cd2852dfce987e64de1b002e8c9e5029f8013d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-03-30 11:45:05 +0530

    lake update, command line dummy

--- LeanLion/QuickSort.lean
+++ LeanLion/QuickSort.lean
@@ -12,4 +12,15 @@ Our implementation of quicksort for lists follows the following steps:
 * Otherwise, let `pivot` be the first element (`head`) of the list.
 * Let `smaller` be the list of elements smaller (`≤`) than `pivot` and `larger` be the list of elements larger (`>`) than `pivot`.
 * Recursively sort `smaller` and `larger` lists and concatenate them with `pivot` in between.
+
+We begin by defining `smaller` and `larger` lists. We define them as abbreviations so that they are automatically unfolded by Lean.
 -/
+
+
+
+
+/-!
+We define `smaller` and `larger` using `List.filter`. We need some theorems relating to filtering.
+-/
+#check List.mem_filter -- ∀ {α : Type u_1} {x : α} {p : α → Bool} {as : List α}, x ∈ List.filter p as ↔ x ∈ as ∧ p x = true
+#check List.length_filter_le -- ∀ {α : Type u_1} (p : α → Bool) (l : List α), List.length (List.filter p l) ≤ List.length l
