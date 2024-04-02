93495260dea0c080504a05b0f5ba1133255e9e5a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-04-02 14:10:35 +0530

    skeleton for auto tactic

--- LeanLion/Examples.lean
+++ LeanLion/Examples.lean
@@ -1,4 +1,28 @@
-import Mathlib
 import LeanAideTools
+/-!
+## Tactics in the background
 
-#autos [omega, linarith, ring, norm_num]
+First we see in a simple example
+-/
+
+example : 1 ≤ 2 := by sorry
+
+/-!
+We next illustrate the use of the `byy` tactic where the proof cannot be found straightaway.
+-/
+
+opaque sillyN : Nat
+axiom silly : sillyN = 2
+
+example : sillyN ≤ 3 := by sorry
+
+/-!
+We now show that stuff keeps running in the background
+-/
+set_option aided_by.delay 0 in
+example : sillyN ≤ 7 := by sorry
+
+/-!
+We do not continue within `case` and `match` blocks. So there is an alternative way to use `byy` in the following example.
+-/
+example (n : Nat) : n/2 ≤ n := by sorry
