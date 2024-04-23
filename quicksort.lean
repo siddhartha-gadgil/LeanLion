fbebb9b3fb487f29816fdd7a0b33e6bc90846792
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-04-23 07:38:15 +0530

    cli uses async code

--- quicksort.lean
+++ quicksort.lean
@@ -1,15 +1,15 @@
-import LeanLion.QuickSort
+import LeanLion.FastQuickSort
 
 def main (args: List String) : IO Unit := do
   let n := args.head? |>.bind String.toNat? |>.getD 100000
   let m := args[1]? |>.bind String.toNat? |>.getD (n * 10)
 
   let l ←  List.range n |>.mapM fun _ => IO.rand 0 m
-
+  let l := l.toArray
   IO.println s!"Sorting random list of size {n} with max value {m}"
   let start ← IO.monoMsNow
-  let sortedList :=  l -- Do the sorting here
+  let sorted := quickSortAsync l 3
   let done ← IO.monoMsNow
   IO.println s!"Time for sorting: {done - start}ms"
-  IO.println s!"Done: got {sortedList.length} elements. First 10: {sortedList.take 10}"
+  IO.println s!"Done: got {sorted.size} elements. First 10: {sorted.toList.take 10}"
   return ()
