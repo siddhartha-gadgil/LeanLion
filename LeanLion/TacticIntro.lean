f34cdec3b4c8eaedf24a3f2a920f522bec3bd4b3
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-01 18:21:48 +0530

    draft of lecture

--- LeanLion/TacticIntro.lean
+++ LeanLion/TacticIntro.lean
@@ -2,6 +2,16 @@ import Lean
 import Mathlib
 open Lean Meta Elab Tactic
 
+elab "#stx" t:term : command => do
+  logInfo m!"Syntax: {t} is {repr t}"
+
+elab "#expr" t:term : command =>
+  Command.liftTermElabM do
+  let t ← Term.elabTerm t none
+  logInfo m!"Expression: {t} is {repr t}"
+  let t ← reduce t
+  logInfo m!"Reduced: {t} is {repr t}"
+
 /-!
 # Meta-programming for Tactics
 
@@ -18,7 +28,11 @@ open Lean Meta Elab Tactic
 * Lean code is processed in two steps:
   * **Parsing** - the text is converted into a `Syntax` tree. This could represent a term, a tactic, a command, an identifier etc.
   * **Elaboration** - the `Syntax` tree for a *term* is converted into an `Expr` tree. This is the internal representation in the foundations of Lean. We also elaborate *commands*.
+-/
+#stx 2 + 4
+#expr (2 : Nat) + 4
 
+/-!
 ## Metaprogramming with Syntax
 
 * The syntax of Lean is extensible.
@@ -50,13 +64,51 @@ theorem eg₁ : 2 ≤ 4 := by
   repeat_apply Nat.succ_le_succ then Nat.zero_le
 
 #print eg₁
-
 /-!
 ## What are tactics?
 
 * An expression of the form `by $tacticSeq` is just syntax for a term.
 * If the tactic sequence is valid and of the correct type, it is elaborated into a term of the specified type.
+* In interactive mode, at each step we have a *tactic state* consisting of:
+  - A list of goals, each with a target type.
+  - A local context of hypotheses and definitions.
+  - A set of metavariables (including the goals).
+* A tactic is essentially a function that takes a tactic state and returns a new tactic state.
+* More formally, a tactic is a term of type `TacticM Unit`.
+
+## State Monads
+
+* If `BlahM` is a *state monad* with state `Blah.State`, then a term of type `BlahM α` corresponds to:
+  - Given an initial state in `Blah.State`
+  - Returns a value of type `α`
+  - and a final state in `Blah.State`.
+* Thus, this is essentially a function `Blah.State → α × Blah.State`.
+* The monadic structure and `do` notation allow us to
+  - Compose in a convenient way.
+  - Automatically handle the state.
+* A term of type `α` can be turned into a term of type `BlahM α` by using `pure α: s ↦ (α, s)`, i.e., ignoring and fixing the state (`return` is roughly a synonym for pure).
+* In general, given a type `s` (the state), we have a state monad `State s α`
+
+## The Monad `TacticM`
+
+* To first approximation, `TacticM` is a state monad with state `Tactic.State`.
+* Hence, a term of type `TacticM Unit` is a function that:
+  - Given a `Tactic.State` (the tactic state)
+  - Returns a value of type `Unit`: the unique value of type `Unit`.
+  - and a new `Tactic.State`.
+* Thus, a tactis is a function that takes a tactic state and returns a new tactic state.
+
+## More on `TacticM`
+
+* We in fact have a hierarchy of Monads, `CoreM`, `MetaM`, `TermElabM`, `TacticM`.
+* So, to the next approximation, `TacticM α = State Tactic.State (TermElabM α)`, `TermElabM α = State Term.State (MetaM α)` etc.
+* A term in a lower monad can be lifted to a higher monad using compositions of `pure`.
+* Actually all these monads are state monads equipped with a `ReaderT`, which means they also have a **read-only `Context`**.
+* There are a few other subtleties which we can ignore for now.
+
+## First examples
 -/
+#print TacticM
 
 elab "evil" : tactic => do
   setGoals []
@@ -64,7 +116,7 @@ elab "evil" : tactic => do
 -- example : 3 ≤ 5 := by
 --   evil
 
-#check sorryAx
+#check sorryAx -- (α : Sort u) → optParam Bool false → α
 
 elab "scowl" s:str : tactic => do
   logInfo m!"Message: {s}"
@@ -75,6 +127,11 @@ elab "scowl" s:str : tactic => do
 example : 3 ≤ 5 := by
   scowl "This is a bad proof"
 
+/-!
+### Next example: `use_till`
+
+We write a tactic to plug in various natural numbers into `use` until the tactic followed by another succeeds.
+-/
 elab "use_till" n:num "then" tac:tacticSeq : tactic => withMainContext do
   let n := n.getNat
   let s ← saveState
@@ -97,9 +154,23 @@ elab "use_till" n:num "then" tac:tacticSeq : tactic => withMainContext do
 example : ∃ n: Nat, n * n = 64 := by
   use_till 10 then rfl
 
+/-!
+### Next example: `le_rw`
+
+We now give a more substantial example. This can be done with just macros, but we will use metaprogramming to illustrate the principles.
+
+The tactic `rw_le h` works if the goal is of the form `a ≤ b` and `h` is a proof of `c ≤ d`, with `a`, `b`, `c` and `d` all natural numbers. If `a = c` or `b = d` or both, then it rewrites the goal.
+
+We do this in three steps:
+
+* Recognize when an expression is of the form `a ≤ b`.
+* Write a tactic that says whether the goal is of the form `a ≤ b`.
+* Write a tactic that rewrites the goal if the goal is of the form `a ≤ b` and the argument is a proof of `c ≤ d` with at least one end matching.
+-/
 #check Nat.le
 
-def matchLe (e: Expr) : MetaM (Option (Expr × Expr)) := do
+def matchLe (e: Expr) : MetaM (Option (Expr × Expr)) :=
+  do
   let nat := mkConst ``Nat
   let a ← mkFreshExprMVar nat
   let b ← mkFreshExprMVar nat
@@ -123,7 +194,7 @@ example (x y : Nat) (h : x ≤ y) : x ≤ y :=
     assumption
 
 elab "rw_le" t:term : tactic => withMainContext do
-  let e ← Tactic.elabTerm t (some (mkConst ``Nat))
+  let e ← Tactic.elabTerm t none
   let p₁? ← matchLe <| ← inferType e
   let p₂? ← matchLe (← getMainTarget)
   match p₁?, p₂? with
