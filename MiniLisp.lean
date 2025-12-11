df8a2e6a9dcad3343c6cb19ad5776b9976194734
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-12-11 15:44:34 +0000

    more lisp

--- MiniLisp.lean
+++ MiniLisp.lean
@@ -5,7 +5,30 @@ inductive LispVal
   | natVal (n : Nat) : LispVal
   | boolVal (b : Bool) : LispVal
   | invalid : LispVal
-  deriving Repr
+  deriving Repr, Inhabited
+
+abbrev LispM := StateT  (Std.HashMap Name LispVal) MetaM
+
+def LispM.addVar (name : Name) (val : LispVal) :
+    LispM Unit := do
+  modify (·.insert name val)
+
+def LispM.eraseVar (name : Name) :
+    LispM Unit := do
+  modify (·.erase name)
+
+def LispVal.add : LispVal → LispVal → LispVal
+  | LispVal.natVal n1, LispVal.natVal n2 => LispVal.natVal (n1 + n2)
+  | _, _ => LispVal.invalid
+
+def LispVal.lt : LispVal → LispVal → LispVal
+  | LispVal.natVal n1, LispVal.natVal n2 => LispVal.boolVal (n1 < n2)
+  | _, _ => LispVal.invalid
+
+def LispVal.ifThenElse : LispVal → LispVal → LispVal → LispVal
+  | LispVal.boolVal true, thenBranch, _ => thenBranch
+  | LispVal.boolVal false, _, elseBranch => elseBranch
+  | _, _, _ => LispVal.invalid
 
 declare_syntax_cat lisp_expr
 
@@ -23,35 +46,49 @@ syntax "(" "<" lisp_expr lisp_expr ")" : lisp_expr
 syntax "(" "if" lisp_expr lisp_expr lisp_expr ")"
   : lisp_expr
 
-mutual
-  partial def getNatFromLispExpr : TSyntax `lisp_expr → MetaM Nat
-  | `(lisp_expr| $n:num) => return n.getNat
+syntax "(" "let" ident lisp_expr lisp_expr ")"
+  : lisp_expr
+
+syntax ident : lisp_expr
+
+partial def parseLispVal : TSyntax `lisp_expr → LispM LispVal
+  | `(lisp_expr| T) => return LispVal.boolVal true
+  | `(lisp_expr| NIL) => return LispVal.boolVal false
+  | `(lisp_expr| $n:num) => return LispVal.natVal n.getNat
   | `(lisp_expr| (+ $xs*)) => do
-    let ns ← xs.mapM getNatFromLispExpr
-    return ns.foldl (· + ·) 0
-  | `(lisp_expr| (if $cond $thenBranch $elseBranch)) => do
-    let b ← getBoolFromLispExpr cond
-    if b then
-      getNatFromLispExpr thenBranch
-    else
-      getNatFromLispExpr elseBranch
-  | _ => Elab.throwUnsupportedSyntax
-
-  partial def getBoolFromLispExpr : TSyntax `lisp_expr → MetaM Bool
-  | `(lisp_expr| T) => return true
-  | `(lisp_expr| NIL) => return false
+    let vals ← xs.mapM parseLispVal
+    return vals.foldl (·.add ·) (LispVal.natVal 0)
   | `(lisp_expr| (< $x $y)) => do
-    let nx ← getNatFromLispExpr x
-    let ny ← getNatFromLispExpr y
-    return nx < ny
+    let vx ← parseLispVal x
+    let vy ← parseLispVal y
+    return vx.lt vy
+  | `(lisp_expr| (if $cond $thenBranch $elseBranch)) => do
+    let vcond ← parseLispVal cond
+    let vthen ← parseLispVal thenBranch
+    let velse ← parseLispVal elseBranch
+    return vcond.ifThenElse vthen velse
+  | `(lisp_expr| (let $id:ident $val $body)) =>   do
+    let vval ← parseLispVal val
+    LispM.addVar id.getId vval
+    let result ← parseLispVal body
+    LispM.eraseVar id.getId
+    return result
+  | `(lisp_expr| $id:ident) => do
+    let env ← get
+    match env.get? id.getId with
+    | some v => return v
+    | none => return LispVal.invalid
   | _ => Elab.throwUnsupportedSyntax
-end
 
 
-elab "lisp_nat%" n:lisp_expr : term => do
-  let nat ← getNatFromLispExpr n
-  return toExpr nat
+elab "lisp%" e:lisp_expr : term => do
+  let lispVal ← (parseLispVal e).run' {}
+  match lispVal with
+  | LispVal.natVal n => return toExpr n
+  | LispVal.boolVal b => return toExpr b
+  | LispVal.invalid => Elab.throwUnsupportedSyntax
 
-#eval lisp_nat% (+ 3 4 (+ 5 3)) -- 15
+#eval lisp% (+ 3 4 (+ 5 3)) -- 15
+#eval lisp% (if (< (+ 2 3) (+ 4 5)) (+ 10 20) (+ 30 40)) -- 30
 
-#eval lisp_nat% (if (< (+ 2 3) (+ 4 5)) (+ 10 20) (+ 30 40)) -- 30
+#eval lisp% (let x (+ 2 3) (let y (+ x 4) (+ y 5))) -- 14
